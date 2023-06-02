c092ef5f614171bb0d04015985ee5e61a79ddd1a
Anna <69199501+Nikitavlasenko81@users.noreply.github.com>
2023-06-02 17:17:12 +0300

    added homework

---
+++ hw-lesson_3.js
@@ -0,0 +1,108 @@
+//1.Виведіть в консоль числа від 1 до n використовуючи цикл for. (Дано: n = 10)
+
+// const startOfNumbers = 1;            
+// for (let i=startOfNumbers;i<11;i++){
+//   console.log(i)
+// }
+
+
+
+//2.Напишіть цикл, який обчислює суму чисел від 1 до n. (Дано: n = 100)
+
+// const startOfNumbers = 1;            
+// let counter = 0;
+// for (let i=startOfNumbers;i<11;i++){
+//      console.log(counter+=i);
+// }
+
+
+
+//3.Використовуючи цикл while, виведіть в консоль числа від n до 1. (Дано: n = 10)
+
+// const startOfNumbers = 1;                            
+// let i=startOfNumbers;
+// while (i){
+//   console.log(i)
+//   i++;
+//   if (i>10){
+//     break;
+//   }
+// }
+
+
+
+//4.Напишіть цикл, який виводить всі парні числа від 1 до n. (Дано: n = 50)
+
+// const startOfNumbers = 1;              
+// for (i=startOfNumbers; i<51; i++){
+//   if(i%2===0){
+//     console.log(i)
+//   }
+// }
+
+
+
+// //5.Виведіть в консоль таблицю множення від 1 до n використовуючи вкладені цикли. (Дано: n = 10)
+// const startOfNumbers = 1;               
+// for (let i=startOfNumbers; i<11; i++){
+//   for (let x=startOfNumbers; x<11; x++)
+//   console.log(x*i)
+// }
+
+
+
+
+//6.Обчисліть факторіал числа n, використовуючи цикл. (Дано: n = 7)
+
+// const startOfNumbers = 1;         
+// let counter = 1;
+// for (i=startOfNumbers;i<8;i++){
+//   console.log (counter*=i)
+// }
+
+
+
+//7. Виведіть перші n чисел послідовності Фібоначчі. (Дано: n = 10) NOT DONE
+
+
+
+//8.Виведіть всі прості числа від 1 до n. (Дано: n = 100)
+
+// function isPrimes(num){
+//     for (i = 2; i < num; i++){
+//         if (num%i===0){
+//             return false
+//         }
+//         return num > 1
+// }
+// }
+
+//   for (let i = 1; i < 101; i++){
+//     if (isPrimes(i)){
+//         console.log(i)
+//     }
+//   }
+
+
+
+  
+//9. Створіть масив чисел від 1 до n, а потім виведіть тільки ті числа, що діляться на 5 без остачі. (Дано: n = 100)
+
+// const startOfNumbers = 1;
+// let i = [];
+// for(i = startOfNumbers; i<101;i++){
+//   if (i%5===0){
+//     console.log (i)
+//   }
+//  }
+
+
+
+//10.Дано масив чисел. Обчисліть суму чисел в цьому масиві. (Дано: масив = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+// const startOfNumbers = 1;
+// let counter = 0;
+// let i = [1,2,3,4,5,6,7,8,9,10];
+// for (i=startOfNumbers;i<11;i++){
+//   console.log (counter+=i)
+// }
+ 
